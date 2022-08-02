d71c547465f8c43cd989b1dd331c6d40bcdcb7df
jsk33a <jsk33a@gmail.com>
2022-08-02 19:31:03 +0900

    1085

--- src/solvedAC/Boj_1085_직사각형탈출.java
+++ src/solvedAC/Boj_1085_직사각형탈출.java
@@ -1,9 +1,30 @@
 package solvedAC;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Boj_1085_직사각형탈출 {
 	
-	public static void main(String[] args) {
-		System.out.println("되냐?");
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+		int x = 0, y = 0, w = 0, h = 0;
+		x = Integer.parseInt(st.nextToken());
+		y = Integer.parseInt(st.nextToken());
+		w = Integer.parseInt(st.nextToken());
+		h = Integer.parseInt(st.nextToken());
+
+		
+		int minV = Integer.MAX_VALUE;
+		minV = (minV > x)? x: minV;
+		minV = (minV > y)? y: minV;
+		minV = (minV > w-x)? w-x: minV;
+		minV = (minV > h-y)? h-y: minV;
+		
+		System.out.println(minV);
+		
 	}
 
 }
